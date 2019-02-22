a0f43e537f53866bdeefb77f404b04aed4920feb
Sergio Garcia <sergiogcx@gmail.com>
2019-02-22 16:33:18 -0600

    Added multiple endpoint support

--- src/components/RegisterFunctions.js
+++ src/components/RegisterFunctions.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
 
 export const register = newUser => {
+
+    console.log("We will need to add environment variables");
+
+    const endpoint = process.env.NODE_ENV !== 'production' ? 
+                     process.env.REACT_APP_API_URL : // Set up at build time
+                     "http://localhost:3001"; // Default local dev
+
+    console.log("Current endpoint: " + endpoint);
+
     return axios
-        .post('/users/register', {
+        .post(endpoint + '/users/register', {
             first_name: newUser.first_name,
             last_name: newUser.last_name,
             org: newUser.org,
@@ -13,4 +22,3 @@ export const register = newUser => {
             console.log('Reistered')
         })
 }
-
