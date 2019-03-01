6061ed1a3e1d4883c5e61106701a2fbd6df1a624
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2019-03-01 13:15:01 -0600

    adding endpoint api

--- src/components/RegisterFunctions.js
+++ src/components/RegisterFunctions.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
+import API from './Api'
 
 export const register = newUser => {
     // If defined, use the API_URL endpoint
-    const endpoint = process.env.REACT_APP_API_URL || "http://localhost:3001";
-    return axios
-        .post(endpoint + '/users/register', {
+    // const endpoint = process.env.REACT_APP_API_URL || "http://localhost:3001";
+    // return axios
+        //.post(endpoint + '/users/register', {
+    return API.post('/users/register', {
             first_name: newUser.first_name,
             last_name: newUser.last_name,
             org: newUser.org,
