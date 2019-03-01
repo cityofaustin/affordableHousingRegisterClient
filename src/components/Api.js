9a6d0142c0b5c1ed5e1bac8ac13856fb5ff1b6cb
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2019-03-01 13:31:33 -0600

    update baseURL

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   //baseURL: `http://localhost:3001`
-  baseURL: `process.env.REACT_APP_API_URL || http://localhost:3001`
+  //baseURL: `process.env.REACT_APP_API_URL || http://localhost:3001`
+  baseURL: `process.env.REACT_APP_API_URL`
 });
