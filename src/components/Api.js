a123b59243a35c396b4f58e14673f5248b84d673
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2019-03-01 21:06:09 -0600

    Update Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,7 +3,5 @@ require('dotenv').config()
 
 export default axios.create({
   //baseURL: `http://localhost:3001`
-  //baseURL: `process.env.REACT_APP_API_URL || http://localhost:3001`
-  //baseURL: `process.env.REACT_APP_API_URL`
-  baseURL: `http://ec2-52-33-129-5.us-west-2.compute.amazonaws.com`
+  baseURL: process.env.REACT_APP_API_URL || `http://localhost:3001`
 });
